b307d104ba2a03709a6a260d843d1493bad53bb7
mohammad667 <mhmdshmm@gmail.com>
2017-08-26 03:06:09 -0400

    second commit

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,16 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage =[]
+    this.add = function(value){
+    	this._storage.push(value) ;
+    	return this._storage
       // write me
+
+
+
     };
-    this.remove = function() {
+    this.remove = function() { 
+    	this._storage.shift()
+    	return this._storage 
       // write me
     };
   };
