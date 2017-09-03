0e851e08aea972807aa08d302a6b56dedcbd1f2c
mohammad667 <30746219+mohammad667@users.noreply.github.com>
2017-09-03 17:06:08 +0300

    Update js-array-extensions.js

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,21 @@
 // your code here
+var Array = function(arr) {
+  var someInstance = {} ;
+  someInstance.arr = arr ;
+ extend(someInstance, arrayMethods) ;
+  return someInstance ;
+}
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+}
+var arrayMethods = {} ;
+arrayMethods.first = function() {
+  return this.arr[0] ;
+};
+arrayMethods.last = function() {
+  return this.arr[this.arr.length-1] ;
+};
+
